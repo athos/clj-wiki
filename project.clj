3c9e50dfa6c8ead14b7d2059ac13644c9def9c5f
OHTA Shogo <athos0220@gmail.com>
2011-11-09 14:41:23 +0900

    wiki skeleton

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject clj-wiki "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.5"]
+                 [hiccup "0.3.7"]]
   :dev-dependencies [[appengine-magic "0.4.5"]])
--- src/clj_wiki/core.clj
+++ src/clj_wiki/core.clj
@@ -1,11 +1,55 @@
 (ns clj-wiki.core
+  (:use compojure.core
+        hiccup.core)
   (:require [appengine-magic.core :as ae]))
 
+(defn page->html [title content & {:keys [show-edit? show-all?]}]
+  (html [:html
+         [:head [:title title]
+          [:body
+           [:h1 title]
+           [:div {:align "right"}
+            [:a {:href "/"} "[トップ]"]
+            [:a {:href (str "/" title "/edit")} "[編集]"]
+            [:a {:href (str "/all")} "[一覧]"]]
+           [:hr]
+           content]]]))
 
-(defn clj-wiki-app-handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world!"})
+(defn view-page [pagename]
+  (page->html pagename ""))
 
+(defn edit-page [pagename]
+  (page->html
+   pagename
+   [:form {:method "POST" :action (str "/" pagename "/submit")}
+    [:textarea {:name "content" :rows 25 :cols 60} "the content goes here"]
+    [:input {:type "submit" :name "submit" :value "Submit"}]
+    [:input {:type "reset" :name "reset" :value "Reset"}]]))
+
+(defroutes clj-wiki-app-handler
+  (GET "/" req
+       {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body "Hello, world!"})
+  (GET "/all" req
+       {:status 200
+        :headers {"Content-Type" "text/plain"}
+        :body "page list goes here"})
+  (GET "/:wikiname" [wikiname]
+       {:status 200
+        :headers {"Content-Type" "text/html"}
+        :body (view-page wikiname)})
+  (GET "/:wikiname/edit" [wikiname]
+       {:status 200
+        :headers {"Content-Type" "text/html"}
+        :body (edit-page wikiname)})
+  (POST "/:wikiname/submit" [wikiname]
+        {:status 200
+         :headers {"Content-Type" "text/plain"}
+         :body (format "Submit Page for %s" wikiname)})
+  (ANY "*" _
+       {:status 404
+        :headers {"Content-Type" "text/plain"}
+        :body "not found"}))
 
 (ae/def-appengine-app clj-wiki-app #'clj-wiki-app-handler)
