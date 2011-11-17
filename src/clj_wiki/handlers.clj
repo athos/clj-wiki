1e27fa19bc4979e082b83c628b93c3fea6b25257
OHTA Shogo <athos0220@gmail.com>
2011-11-18 06:15:27 +0900

    prepare for introducing DB code

--- src/clj_wiki/handlers.clj
+++ src/clj_wiki/handlers.clj
@@ -1,6 +1,7 @@
 (ns clj-wiki.handlers
   (:use [hiccup.core :only [html]]
-        [ring.util.response :only [redirect-after-post]]))
+        [ring.util.response :only [redirect-after-post]]
+        [somnium.congomongo :only [insert! fetch-one]]))
 
 (defn- page->html [title content & {:keys [show-edit? show-all?]}]
   (html [:html
@@ -14,14 +15,14 @@
            [:hr]
            content]]]))
 
-(defn- render-view-page [pagename]
-  (page->html pagename ""))
+(defn- render-view-page [pagename content]
+  (page->html pagename content))
 
-(defn- render-edit-page [pagename]
+(defn- render-edit-page [pagename content]
   (page->html
    pagename
    [:form {:method "POST" :action "/submit"}
-    [:textarea {:name "content" :rows 25 :cols 60} "the content goes here"]
+    [:textarea {:name "content" :rows 25 :cols 60} content]
     [:input {:type "hidden" :name "wikiname" :value pagename}]
     [:input {:type "submit" :name "submit" :value "Submit"}]
     [:input {:type "reset" :name "reset" :value "Reset"}]]))
@@ -31,19 +32,22 @@
    :headers {"Content-Type" "text/html"}
    :body content})
 
+(defn not-found-page []
+  {:status 404
+   :headers {"Content-Type" "text/plain"}
+   :body "not found"})
+
 (defn view-page [wikiname]
-  (standard-page (render-view-page wikiname)))
+  (if-let [page {:content "hogehoge"} #_(fetch-one :pages :where {:name wikiname})]
+    (standard-page (render-view-page wikiname (:content page)))
+    (not-found-page)))
 
 (defn list-page []
   (standard-page "page list goes here"))
 
 (defn edit-page [wikiname]
-  (standard-page (render-edit-page wikiname)))
+  (let [page {:content "hogehoge"} #_(fetch-one :pages :where {:name wikiname})]
+    (standard-page (render-edit-page wikiname (:content page)))))
 
 (defn commit-page [wikiname content]
   (redirect-after-post (str "/" wikiname)))
-
-(defn not-found-page []
-  {:status 404
-   :headers {"Content-Type" "text/plain"}
-   :body "not found"})
